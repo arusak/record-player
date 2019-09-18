646e283dbdda24aa5ae41690a778f7b9e37c37ab
arusak <anton.rusak@gmail.com>
2019-09-18 16:02:54 +0300

    Added coverage CI job (coveralls.io)

---
+++ src/tests/record-player.test.js
@@ -0,0 +1,8 @@
+import {RecordPlayer} from '../record-player.js';
+
+describe('RecordPlayer', function () {
+    it('should construct itself', () => {
+        let player = new RecordPlayer(document.body);
+        expect(player).toBeDefined();
+    })
+});
---
+++ src/tests/seeker.test.js
@@ -0,0 +1,8 @@
+import {Seeker} from '../seeker.js';
+
+describe('Seeker', function () {
+    it('should construct itself', () => {
+        let seeker = new Seeker();
+        expect(seeker).toBeDefined();
+    })
+});
